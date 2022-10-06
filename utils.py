c9c0e95c62ef02f6e58762e9edade07ec64b82b9
ZhanZhynn <91299326+ZhanZhynn@users.noreply.github.com>
2022-10-06 18:49:03 +0800

    added noise attack

--- utils.py
+++ utils.py
@@ -15,6 +15,18 @@ def fgsm(model, x, t, loss_func, eps, min=0, max=1):
 
     return Variable(torch.clamp(x.data + eps * torch.sign(x.grad.data), min=min, max=max))
 
+#Implemented Noise Attack
+def noise_attack(model, x, t, loss_func, eps, min=0, max=1):
+    if not isinstance(x, Variable):
+        x, t = Variable(x.cuda(), requires_grad=True), Variable(t.cuda())
+    x.requires_grad = True
+    y = model(x)
+    loss = loss_func(y, t)
+    model.zero_grad()
+    loss.backward(retain_graph=True)
+    
+    return Variable(x*torch.rand_like(x) * eps) #noise attack
+
 
 def accuracy(y, t):
     pred = y.data.max(1, keepdim=True)[1]
